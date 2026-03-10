425b03272d5577920568b217e11d2fcdf95736f4
mgh8173 <mgh8173@gmail.com>
2026-03-10 06:20:52 +0000

    ci: restore github pages deployment config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions && process.env.GITHUB_REPOSITORY) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig: NextConfig = {
-  reactStrictMode: true
+  output: 'export',
+  basePath: basePath || undefined,
+  assetPrefix: assetPrefix || undefined,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
