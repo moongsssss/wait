e74fa8f6c7a1fe8ce28aeb734740d5437e5e2c76
mgh8173 <mgh8173@gmail.com>
2026-03-10 04:49:21 +0000

    fix: make basePath conditional to resolve local 404

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions && process.env.GITHUB_REPOSITORY) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/wait',
+  basePath: basePath || undefined,
+  assetPrefix: assetPrefix || undefined,
   images: {
     unoptimized: true,
   },
